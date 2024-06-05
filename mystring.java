45dc27857f51741c7ba688ac12a869d02c9adc0d
mdaftab41 <mdaftabalam49041@gmail.com>
2024-06-06 00:59:38 +0530

    String

---
+++ mystring.java
@@ -0,0 +1,12 @@
+public class mystring {
+  public static void main(String[] args) {
+      String name1 = "Alam";
+      String name2 = "Aftab";
+      String name3 = name1+name2;
+      System.out.println(name1);
+      System.out.println(name2);
+      System.out.println(name3);
+      System.out.println(name3.length());
+      
+  }
+}
