4fba6ee1221fc6600c171f96765c325b171f7b50
mdaftab41 <mdaftabalam49041@gmail.com>
2024-06-10 23:11:39 +0530

    FibSeries

---
+++ Fibseries.java
@@ -0,0 +1,13 @@
+public class Fibseries {
+    static int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(4));
+    }
+}
+
