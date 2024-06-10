19b0683c648cdf0d62a769748637123c5b122b50
mdaftab41 <mdaftabalam49041@gmail.com>
2024-06-10 23:12:47 +0530

    string

--- immtable.java
+++ immtable.java
@@ -4,5 +4,10 @@ public class immtable {
     System.out.println(str);
     str= "Prashant Malik";
     System.out.println(str);
+    char arr[]= str.toCharArray();// toString is use to change into string
+    str=arr.toString();
+    str= new String(arr);
+    // for loop use karlo print karne ke liye
+    System.out.println(str);
    }
 }
