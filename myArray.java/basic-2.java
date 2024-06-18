74b04f732d8e1268ba9be8e9e3303567fc07fc2c
mdaftab41 <mdaftabalam49041@gmail.com>
2024-06-18 23:20:25 +0530

    sum

--- myArray.java/basic-2.java
+++ myArray.java/basic-2.java
@@ -21,5 +21,27 @@ class Main {
    for(int i = 0; i < age1.length; i++) {
      System.out.println(age1[i]);
    }
+   // program to find the sum of array element 
+   System.out.println("program to find sum osf array element");
+
+   int[] numbers = {2, -9, 0, 5, 12, -25, 22, 9, 8, 12};
+   int sum = 0;
+   Double average;
+   
+   // access all elements using for each loop
+   // add each element in sum
+   for (int number: numbers) {
+     sum += number;
+   }
+  
+   // get the total number of elements
+   int arrayLength = numbers.length;
+
+   // calculate the average
+   // convert the average from int to double
+   average =  ((double)sum / (double)arrayLength);
+
+   System.out.println("Sum = " + sum);
+   System.out.println("Average = " + average);
     }
    }
