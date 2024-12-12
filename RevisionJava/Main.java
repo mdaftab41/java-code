5047022419c7fb268864c192a65663585848fa74
mdaftab41 <mdaftabalam49041@gmail.com>
2024-12-12 12:58:41 +0530

    Starting from the  basic concept of java

---
+++ RevisionJava/Main.java
@@ -0,0 +1,7 @@
+package RevisionJava;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Starting from the basic concept of java  for testing");
+    }
+  }
