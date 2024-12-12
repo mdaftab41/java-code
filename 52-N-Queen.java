fb551827f2650290e4dec87da184a2697f253a1b
mdaftab41 <mdaftabalam49041@gmail.com>
2024-12-12 12:57:44 +0530

    PreviousCode

---
+++ 52-N-Queen.java
@@ -0,0 +1,18 @@
+// class Solution {
+//     boolean board[][];
+//     public int totalNQueen(int n){
+//         board = new boolean[n][n];
+//         return countPos(0,n);//initially count pos is zero
+//     }
+
+//     boolean isSafeToPlaceQueen(int )
+//     int countPos(int row , int n){
+//         if(row== n){
+//             return 1;
+//         }
+//         int count = 0; 
+//         for( int col = 0; col<n ; col++){
+
+//         }
+//     }
+// }
