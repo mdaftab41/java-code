f2d2593d68212e3291f9b38904332e6122be30c1
mdaftab41 <mdaftabalam49041@gmail.com>
2024-12-12 13:08:34 +0530

    Primitive Data type

---
+++ RevisionJava/variable.java
@@ -0,0 +1,17 @@
+package RevisionJava;
+
+public class variable {
+    public static void main(String[] args) {
+        System.out.println( "Working on DataType");
+        System.out.println(" Primitive Data type : int  , double , char , boolean ");
+        int age = 25;
+        double salary = 45000.50;
+        char grade = 'A';
+        boolean isActive = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Active: " + isActive);
+      }
+}
